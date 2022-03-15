b8c56f3851ae4dd03dec7a8c70508076ac9b2660
haroldooliveira <haroldo_oliveira@yahoo.com.br>
2022-03-15 19:14:13 +0100

    code ready to be deployed to production

---
+++ app.py
@@ -0,0 +1,74 @@
+from re import U
+from urllib import request
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+import datetime
+
+date = st.date_input(
+    "Fecha de Viaje",
+    datetime.date(2022, 3, 15))
+
+time = st.time_input('Hora del embarque: ', date.time(17,45))
+
+pickup_longitude = st.number_input('Ingrese latidud inicial')
+pickup_latitude = st.number_input('Ingrese longitud final')
+dropoff_longitude = st.number_input('Ingrese latidud inicial')
+dropoff_latitude = st.number_input('Ingrese longitud final')
+passenger = st.number_input('Ingrese numero de personas')
+
+fare = request.get(url).json()
+
+
+
+st.write(fare)
+
+'''
+
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
